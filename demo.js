000d0b177e47ed38bfb0535d29d3111f15d4a92c
komalsyedage <komalsyedage@gmail.com>
2022-10-04 13:57:32 +0530

    First Program   execution in JS
    First Program   execution in JS

---
+++ demo.js
@@ -0,0 +1,9 @@
+console.log(" My Name : Komal Vitthal Dhone");
+console.log("My Hobbies: 1.Pensil Sketch  2.Cooking  3.Listening Musics");
+console.info("My Personal Strength: 1.Hard Working  2.Self-Motivated  3.disciplned");
+console.warn("My Weakness: 1.Emotional  2.Takes Things Personally  3.Fearful");
+console.log(30+50);
+console.log(49*479);
+
+
+
