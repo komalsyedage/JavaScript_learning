4e70e1038d1bf86551cdc91a66506ff0a2207ce4
komalsyedage <komalsyedage@gmail.com>
2022-10-06 15:19:29 +0530

    demo and assignment 1,2
    demo and assignment 1,2

---
+++ 01_swap_var_assign2.js
@@ -0,0 +1,21 @@
+//swap two variable using Third variable:-
+var sweety;
+sweety="Sweety";
+var cutie;
+cutie= "Cutie";
+console.log("Before swapping:-");
+console.log(sweety,cutie);
+sweety="Cutie";
+temp="Sweety"
+console.log("After swapping:-");
+console.log(sweety,temp);
+//swap three variable values:-
+var    num1=100,num2=200,num3=300;
+console.log("Before swapping:-");
+console.log(num1,num2,num3);
+num1=num2;
+num2=num3;
+num3=num3-num1;
+
+console.log("After swapping:-");
+console.log(num1,num2,num3);
---
+++ 01_vriables.js
@@ -0,0 +1,12 @@
+var my_apple; //varaible decleration
+my_apple ="Strawberry" //variable inetialisation
+var my_name="komal Dhone " //variable declaration and inetialisation is on sameline
+console.log(my_apple);
+console.log(my_name)
+
+
+
+
+
+
+
