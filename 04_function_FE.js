be96303828c744468406f01920fcd56a4760f164
komalsyedage <komalsyedage@gmail.com>
2022-10-10 22:27:42 +0530

    Ass of FE and String
    Ass of FE and String

--- 04_function_FE.js
+++ 04_function_FE.js
@@ -1,6 +1,6 @@
 //function Expression to get square of number
 
-console.log("==========Using Function Expression Find Square of 4 Numbers=========");
+console.log("===================Using Function Expression Find Square of 4 Numbers=========================");
  var square = function(value)  {
 console.log(value*value);
 }
@@ -11,7 +11,7 @@ square(100);
 
 //Find the Type Of Function
 
-console.log("=============Find Type Of Function============");
+console.log("=============================Using FE Find Type Of Function==================================================");
 var rest=function(arg1){
 console.log(typeof arg1);
 }
@@ -23,9 +23,45 @@ rest(null)
 
 
 //Using Function Expression find Area of Triangle
-console.log("================Using Function Expression find Area Of Triangle================")
+console.log("==========================Using Function Expression find Area Of Triangle=========================================")
 var areaof_triangle=function(base,height){
 console.log("Area Of Triangle:",base*height);
 }
 
 areaof_triangle(45,34)
+
+//Using FE find area of Rectangle
+console.log("=======================Area Of Rectangle========================================================");
+var areaOf_rectangle=function(length,width){
+    console.log("Area Of Rectangle",length,width)
+}
+
+
+areaof_triangle(499,917)
+
+
+//Using FE Swap Values
+ var swap_values=function (arg1, arg2) {
+    console.log("=====================================Using FE Before Swap===========================================");
+    console.log(arg1, arg2);
+
+    console.log("=====================================Using FE After Swap============================================");
+    var temp = arg1;
+    arg1 = arg2;
+    arg2 = temp;
+    console.log(arg1, arg2);
+}
+swap_values("Virat", "Anushka")
+swap_values(1000,2000)
+//Using FE find Index,chractor,last chaactor,and middle
+console.log("=============================Using FE Find Position===================================== "); 
+var string=function(string){
+   var count= string.length;
+   console.log(count);
+   //or use this type
+   console.log(string.charAt(8));
+   console.log(string.indexOf());
+}
+
+
+string("JavaScript Is Most Popular Language")
