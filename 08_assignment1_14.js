c2c5ecbae5e55d10aed2eadfaf725a36d3288e05
komalsyedage <komalsyedage@gmail.com>
2022-10-14 12:22:17 +0530

    Ass on if stmt , switch case
    Ass on if stmt , switch case

---
+++ 08_assignment1_14.js
@@ -0,0 +1,22 @@
+for (let num = 5; num <=15;num=num+1){
+    console.log(num);
+   
+}
+for (let num2 = 50; num2 >=40; num2=num2-1) {
+   
+    console.log(num2);
+}
+
+for (let index = 1; index<=30; index=index+2) {
+    console.log(index);
+}
+for (let value = 0; value <=20; value=value+2) {
+    console.log(value);
+}
+for (let college = 5; college <=50; college= college+5) {
+ console.log(college); 
+}
+for (let home = 10; home <=100;home= home+10) {
+   console.log(home);
+    
+}
